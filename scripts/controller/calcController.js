9390dd0648126cc79743890d395aa5cd9daeffc3
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-04 11:59:31 -0300

    aula 17 add: applying multiple Events using split

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -5,9 +5,9 @@ class CalcController {
         this._displayCalcEl =  document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
-
         this.currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize(){
@@ -17,7 +17,25 @@ class CalcController {
         this.setDisplayDateTime();
       }, 1000);
 
+    }
 
+    addEventListenerAll(element, events, fn ){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        })
+    }
+
+    initButtonsEvents(){
+      let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+    
+       buttons.forEach((btn, index) =>{
+        this.addEventListenerAll(btn, "click drag", e => {
+            console.log(btn.className.baseVal.replace("btn-", ""));
+        });
+        this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+            btn.style.cursor = "pointer";
+        });
+       });
     }
 
     setDisplayDateTime(){
