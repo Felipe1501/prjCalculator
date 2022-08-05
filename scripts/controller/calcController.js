031c7fddcb05c2f02a860dd2d674d930e2f139bf
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-05 10:21:51 -0300

    aula 30 fix: handling errors with try catch

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -177,8 +177,14 @@ class CalcController {
     }
 
     getResult(){
-
-       return eval(this._operation.join(""));
+        try{
+            return eval(this._operation.join(""));
+        }catch(e){
+            setTimeout(() =>{
+                this.setError();
+            }, 1);
+        }
+       
     }
 
     calc(){
@@ -414,6 +420,12 @@ class CalcController {
     }
 
     set displayCalc(value){
+
+        if(value.toString().length > 10){
+            this.setError();
+            return false;
+        }
+
         this._displayCalcEl.innerHTML = value;
     }
 
